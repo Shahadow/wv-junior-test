d261632d58d0629c935a7665fb52f9d81780a0c2
Shahadow <shadowskin1@gmail.com>
2021-03-13 03:10:51 +0300

    feat(composer): added composer with dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '45f52366fd994afdee0781b05a38c751531c6f90',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '45f52366fd994afdee0781b05a38c751531c6f90',
+    ),
+    'krugozor/database' => 
+    array (
+      'pretty_version' => 'v2.0',
+      'version' => '2.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a246a85ec7f4bc7640753cdef1dcd4070e256d51',
+    ),
+  ),
+);
